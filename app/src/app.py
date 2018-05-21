2b8fc9712be92ec47e9d9cdc08dc31674de80b9b
Richard Price-Jones <me@richardprice-jones.co.uk>
2018-05-21 01:43:02 +0100

    added args handler

--- app/src/app.py
+++ app/src/app.py
@@ -4,6 +4,7 @@ import sys, json, os
 import elasticsearch as es
 from elasticsearch import helpers
 from config import config
+import argparse
 
 app = Flask(__name__)
 client = es.Elasticsearch(hosts=[{'host': config['db_host'], 'port': config['db_port']}])
@@ -48,14 +49,15 @@ def DELETE_PEOPLE(id):
 #  Get all the people
 @app.route('/people', methods=['GET'])
 def people():
-        # here we want to get the value of user (i.e. ?user=some-value)
-    page = request.args.get('page')
-    print(page)
 
+    page = request.args.get('page')
+
+    # if note page number is given default to page 0
     if page is None:
         page=0
+
     doc = {
-            "from" : page, "size" : 10,
+            "from" : page, "size" : 20,
             'query': {
                 'match_all' : {}
         }
@@ -89,7 +91,7 @@ def loadData(data_file, index_name):
 
     # Create Index and people profile
     try:
-        client.indices.create(index="people", body=body)
+        client.indices.create(index=index_name, body=body)
     except es.exceptions.TransportError as e:
         if e.error != 'index_already_exists_exception':
             raise
@@ -102,21 +104,38 @@ def loadData(data_file, index_name):
 
     for the_instance in json_docs:
             actions.append({
-                "_index": 'people',
+                "_index": index_name,
                 "_type": 'profile',
                 "_source": the_instance
             })
     # Bulk load to btach import the json to elasticsearch, to reduce overhead from making network requests. 
-    r, _ = helpers.bulk(client, actions, index='people', doc_type='profile', refresh=True)
+    r, _ = helpers.bulk(client, actions, index=index_name, doc_type='profile', refresh=True)
     print("Inserted: {} documents".format(r))
 
     
 
 
 if __name__ == '__main__':
-    # Add argument handler here!
-    # Check for a load flag is so then load the data to popluate elasticSearch
 
-    # loadData(sys.argv[1], "people")
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(description='Rest Api for Json data') 
+
+    # Json file to be loaded
+    parser.add_argument('--load', type=str,
+                    help='Json file to be loaded')
+
+    parser.add_argument('--index', type=str,
+                    help='index to add into elastic search')
+
+
+    args = parser.parse_args()
+
+    if args.load is not None and args.index is not None:
+        print(args.load, args.index)
+        loadData(args.load, args.index)
+
     app.run(debug=True,host='0.0.0.0')
+
+
+ 
 
